7842e194f8b36cf67c11638e4750ce3646fbf2ab
Guillaume Pelletier <3820957+gpelletier0@users.noreply.github.com>
2025-07-11 16:21:46 -0400

    Refactor and integrate recipe image generation using Stable Diffusion (#2)
    * Add recipe image generation using Stable Diffusion API
    
    * Integrate RecipeImageGenerator for automatic image creation in `from_lufa_recipe` method
    
    * Update .gitignore to exclude /recipes and /generated_images directories
    
    * Refactor image generation to use `InferenceClient` from Hugging Face and remove `requests` dependency. Update `requirements.txt`.

---
+++ image_generation.py
@@ -0,0 +1,45 @@
+﻿import base64
+import os
+from io import BytesIO
+from huggingface_hub import InferenceClient
+
+
+class RecipeImageGenerator:
+    __prompt = f"A small image for the following recipe description for a paprika recipe manager (https://www.paprikaapp.com/):"
+
+    def __init__(self):
+        self.__stable_diffusion_api_key = os.environ.get("STABLE_DIFFUSION_API_KEY")
+
+    def generate_with_stable_diffusion(self, recipe_name: str) -> str | None:
+        client = InferenceClient(
+            provider="nebius",
+            api_key=self.__stable_diffusion_api_key
+        )
+
+        image = client.text_to_image(
+            f"A beautiful small image of a {recipe_name}",
+            model="stabilityai/stable-diffusion-xl-base-1.0",
+        )
+
+        # self.save_image_to_disk(image, recipe_name)
+
+        return self.pil_to_base64(image)
+
+    @staticmethod
+    def save_image_to_disk(image, recipe_name):
+        image_dir = "generated_images"
+        if not os.path.exists(image_dir):
+            os.makedirs(image_dir)
+
+        safe_filename = "".join(c for c in recipe_name if c.isalnum() or c in (' ', '_')).rstrip().replace(' ', '_')
+        image_path = os.path.join(image_dir, f"{safe_filename}.jpg")
+        image.save(image_path, format='JPEG')
+
+        print(f"Image saved to {image_path}")
+
+    @staticmethod
+    def pil_to_base64(image):
+        buffer = BytesIO()
+        image.save(buffer, format='JPEG')
+
+        return base64.b64encode(buffer.getvalue()).decode('utf-8')
